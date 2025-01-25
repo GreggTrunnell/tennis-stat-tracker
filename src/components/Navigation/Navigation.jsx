96f90f3d9e1e8ca051868b70361be8576fb71ce5
Gregg Trunnell <gggtrunnell@gmail.com>
2025-01-25 00:48:03 -0600

    ready to delete branches

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,17 +4,17 @@ function Navigation( ) {
   
   return (
     <nav className="mb-3"> 
-    <ul class="nav nav-pills">
-      <li class="nav-item">
+    <ul className="nav nav-pills">
+      <li className="nav-item">
         <NavLink className="nav-link" to="/">Home</NavLink> 
       </li>
-      <li class="nav-item">
+      <li className="nav-item">
         <NavLink className="nav-link" to="/search">Search</NavLink> 
       </li>
-      <li class="nav-item">
+      <li className="nav-item">
         <NavLink className="nav-link" to="/match-stats">Match Stats</NavLink> 
       </li>
-      <li class="nav-item">
+      <li className="nav-item">
         <NavLink className="nav-link" to="/add-players">Add Player</NavLink> 
       </li>
     </ul>
