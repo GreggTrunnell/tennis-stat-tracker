e54ad7117bb99af837f96dd984a964b9fea3daf0
Gregg Trunnell <gggtrunnell@gmail.com>
2025-01-26 23:02:47 -0600

    worked on addPlayerOnePoint function

--- src/components/Score/Score.jsx
+++ src/components/Score/Score.jsx
@@ -4,20 +4,40 @@ function Score( ) {
   
   const [ playerOneScore, setPlayerOneScore ] = useState( 0 );
   const [ playerTwoScore, setPlayerTwoScore ] = useState( 0 );
-  
+
+  const tennisScore = [ "Love", "15", "30", "40", "Ad", "Game"];
+  // const isDuece = setPlayerOneScore( 3 ); setPlayerTwoScore( 3 );
+
   const addPlayerOnePoint=()=>{
-    setPlayerOneScore( playerOneScore + 1 );} 
+    if( playerOneScore < 6 )
+      setPlayerOneScore( playerOneScore + 1 );
+    if( playerOneScore === 4 && playerTwoScore === 3)
+      setPlayerOneScore( 5 );
+    if( playerOneScore === 5 && playerTwoScore === 4 )
+      setPlayerOneScore( 3 );
+    if( playerOneScore === 4 && playerTwoScore === 5 )
+      setPlayerOneScore( 3 );
+    if( playerOneScore === 6 )
+      setPlayerOneScore( 100 );
+  }
 
-  const addPlayerTwoPoint=()=>{
-    setPlayerTwoScore( playerTwoScore + 1 );} 
-
+  
+  // const addPlayerTwoPoint=()=>{
+  //   if( playerTwoScore < 4)
+  //     setPlayerTwoScore( playerTwoScore + 1 );} 
+    
+  //   if( playerTwoScore === 4 && playerOneScore === 3 )
+  //     setPlayerTwoScore( 5 );
+    
+console.log( 'playerOneScore',playerOneScore )
   return (
      <div className='Score'>
         <p>Something in Score</p>
-        <button onClick={()=>{ addPlayerOnePoint()}}>Point Player One</button>
-        <p>{ playerOneScore }</p>
-        <p>{ playerTwoScore }</p>
-        <button onClick={()=>{ addPlayerTwoPoint()}}>Point Player One</button>
+        <button onClick={()=>{ addPlayerOnePoint( "playerOne" )}}>Point Player One</button>
+        <p>Player One { playerOneScore }</p>
+        <p>Player Two { playerTwoScore }</p>
+
+        <button onClick={()=>{ addPlayerTwoPoint( "playerTwo" )}}>Point Player Two</button>
     </div>
   );
   }
