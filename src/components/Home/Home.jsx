d37e1b763eadd370face6aefde728823e95928b1
Gregg Trunnell <gggtrunnell@gmail.com>
2025-02-02 00:48:48 -0600

    tweaked home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,9 @@
-import MessagesList from "../MessagesList/MessagesList";
 
 function Home( ) {
   
   return (
      <div className='Home'>
         <img className="w-50" src="public/images/ball_man copy.png"/>
-        <MessagesList />
     </div>
   );
   }
