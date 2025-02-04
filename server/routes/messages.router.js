a6941f378b8f0e8af10c90b9a97946689132c082
Gregg Trunnell <gggtrunnell@gmail.com>
2025-02-04 13:07:09 -0600

    save before making new branch

--- server/routes/messages.router.js
+++ server/routes/messages.router.js
@@ -2,21 +2,31 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+// router.get('/', ( req, res )=>{
+//     console.log( 'GET in router' );
+//     const searchQuery = req.query.q || '';
+//     const queryText = `SELECT players.last_name AS "last name", messages.from AS "from", messages.message AS "message"
+//                         FROM players
+//                         JOIN users ON players.id = users.player_id
+//                         JOIN messages ON messages.id = users.message_id
+//                         WHERE players.last_name ILIKE $1;`;
+//     const values =[`%${ searchQuery }%`];
+
+//     pool.query( queryText, values ).then(( results )=>{
+//         res.send( results.rows );
+//     }).catch(( err )=>{
+//         console.log( err );
+//         res.sendStatus( 400 );  
+//     })
+// })
 router.get('/', ( req, res )=>{
     console.log( 'GET in router' );
-    const searchQuery = req.query.q || '';
-    const queryText = `SELECT players.last_name AS "last name", messages.from AS "from", messages.message AS "message"
-                        FROM players
-                        JOIN users ON players.id = users.player_id
-                        JOIN messages ON messages.id = users.message_id
-                        WHERE players.last_name ILIKE $1;`;
-    const values =[`%${ searchQuery }%`];
-
-    pool.query( queryText, values ).then(( results )=>{
+    const queryText = `SELECT * FROM "messages" ORDER BY id ASC;`;
+    pool.query( queryText ).then(( results )=>{
         res.send( results.rows );
     }).catch(( err )=>{
         console.log( err );
-        res.sendStatus( 400 );  
+        res.sendStatus( 400 );
     })
 })
 
--- src/components/zustand/store.js
+++ src/components/zustand/store.js
@@ -2,7 +2,16 @@
 import { create } from 'zustand';
 
 const useStore = create(( set )=>({
-
+    messages: [ ],
+    getMessages: ()=>{
+        axios.get( '/api/messages' )
+        .then(( response )=>{
+            console.log('back from get in store', response.data)
+            set(( state )=>({ messages: response.data }));
+        }).catch(( err )=>{
+            console.log('error in get store', err );
+        })
+    }
 }))
  
 export default useStore; 
