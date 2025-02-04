a602b7703a7e6280cb68f99c8d0ab80310285a12
Gregg Trunnell <gggtrunnell@gmail.com>
2025-02-03 23:56:54 -0600

    fixed state issues for adding a player

--- src/components/AddPlayer/AddPlayer.jsx
+++ src/components/AddPlayer/AddPlayer.jsx
@@ -1,37 +1,28 @@
-import useStore from "../zustand/store";
 import axios from "axios";
+import { useState } from "react";
 
 function AddPlayer( ) {
-  
-  const player=useStore(( state )=> state.player );
 
-  function addPlayerProfile(e){
-    console.log('addPlayerProfile hit')
-    const objectToSend={
-      first_name: player.first_name,
-      last_name: player.last_name,
-      playing_hand: player.playing_hand,
-      racquet_brand: player.racquet_brand,
-      country: player.country
-    }
-    axios.post('/api/stats', objectToSend).then(( response )=>{
-      console.log('response', response.data );
-      alert('stats sent')
+  const [ newPlayer, setNewPlayer ] = useState({ first_name: '', last_name: '', playing_hand: '', racquet_brand: '', country: ''})
+
+  function addPlayerProfile(){;
+    axios.post( '/api/stats', newPlayer).then (( response )=>{
+     // console.log('response data from createnew post', response.data )
     }).catch(( err )=>{
-      console.log( 'error in post AddPlayer', err );
+     console.log( err );
     })
-    };
+   }
     
   return (
      <div className='AddPlayer'>
       <img className="w-50 h-50" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGVhfwFVx_VaJ3LP-KCdE6xdSWAs6C9_qaAw&s"/>
       <h2>Add Player Profile</h2>
-     <form > 
-      <input type="text" placeholder="First Name" onChange={ (e)=>player.first_name= e.target.value  } />
-      <input type="text" placeholder="Last Name" onChange={ (e)=>player.last_name= e.target.value  } />
-      <input type="text" placeholder="Playing Hand" onChange={ (e)=>player.playing_hand= e.target.value  } />
-      <input type="text" placeholder="Racquet Brand" onChange={ (e)=>player.racquet_brand= e.target.value  } />
-      <input type="text" placeholder="Country" onChange={ (e)=>player.country= e.target.value  } />
+     <form>
+      <input type="text" placeholder="First Name" onChange={(e)=>{ setNewPlayer({ ...newPlayer, first_name: e.target.value })}} />
+      <input type="text" placeholder="Last Name" onChange={(e)=>{ setNewPlayer({ ...newPlayer, last_name: e.target.value })}} />
+      <input type="text" placeholder="Playing Hand" onChange={(e)=>{ setNewPlayer({ ...newPlayer, playing_hand: e.target.value })}} />
+      <input type="text" placeholder="Racquet Brand" onChange={(e)=>{ setNewPlayer({ ...newPlayer, racquet_brand: e.target.value })}} />
+      <input type="text" placeholder="Country" onChange={(e)=>{ setNewPlayer({ ...newPlayer, country: e.target.value })}} />
       <button onClick={ addPlayerProfile }>Add Player Profile</button>
      </form>
     </div>
--- src/components/zustand/store.js
+++ src/components/zustand/store.js
@@ -1,24 +1,8 @@
-import axios from 'axios';
+
 import { create } from 'zustand';
 
 const useStore = create(( set )=>({
-    player: {
-      first_name: '',
-      last_name: '',
-      playing_hand: '',
-      racquet_brand: '',
-      country: ''
-    // },
-    // messages: [],
-    // fetchMessges: ()=>{
-    //   axios.get( '/api/messages' )
-    //   .then(( response )=>{
-    //     console.log('get from store', response.data )
-    //     set(( state )=> ({ messages: response.data }));
-    //   }).catch(( err )=>{
-    //     console.log('error on get in store', err )
-    //   })
-    }
+
 }))
  
 export default useStore; 
